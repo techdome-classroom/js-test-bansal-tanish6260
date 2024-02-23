342cd63a48e2b39f4d471c3f33ada54866c1e7f2
tanish6260 <161005372+tanish6260@users.noreply.github.com>
2024-02-22 23:58:02 -0800

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,22 +1,28 @@
 
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        m = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+var romanToInt = function(s) {
+      const sym = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const cur = sym[s[i]];
+        const next = sym[s[i + 1]];
+
+        if (cur < next) {
+            result += next - cur;
+            i++;
+        } else {
+            result += cur;
         }
-        
-        ans = 0
-        
-        for i in range(len(s)):
-            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
-                ans -= m[s[i]]
-            else:
-                ans += m[s[i]]
-        
-        return ans
+    }
+
+    return result;
+};
